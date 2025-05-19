01ad93289c0cda09a552b37946d9f23e88820c7f
grahanoi <grahanoi@students.zhaw.ch>
2025-05-19 11:08:40 +0200

    motor_driver angepasst

--- ROSArduinoBridge/motor_driver.h
+++ ROSArduinoBridge/motor_driver.h
@@ -1,16 +1,13 @@
 /***************************************************************
-   Motor driver function definitions - by James Nugen
-   *************************************************************/
+   Motor driver function definitions - für L298, 1 Motor
+***************************************************************/
 
 #ifdef L298_MOTOR_DRIVER
-  #define RIGHT_MOTOR_BACKWARD 5
   #define LEFT_MOTOR_BACKWARD  3
-  #define RIGHT_MOTOR_FORWARD  9
   #define LEFT_MOTOR_FORWARD   4
-  #define RIGHT_MOTOR_ENABLE 12
-  #define LEFT_MOTOR_ENABLE 5
+  #define LEFT_MOTOR_ENABLE    5
 #endif
 
 void initMotorController();
-void setMotorSpeed(int i, int spd);
-void setMotorSpeeds(int leftSpeed, int rightSpeed);
+void setMotorSpeed(int speed);     // für einen Motor
+void setMotorSpeeds(int speed);    // Wrapper für PID-System
